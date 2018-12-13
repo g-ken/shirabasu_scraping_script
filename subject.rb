b6a8f1d36805b24fe35bd98808480116ce9a51db
Riki Ueno <31506@toyota.kosen-ac.jp>
2018-12-13 17:37:28 +0900

    get and parse html chage to method

--- subject.rb
+++ subject.rb
@@ -4,54 +4,45 @@ require 'csv'
 require 'uri'
 require 'benchmark'
 
-def get_school
-  #puts %(starting get_school_id)
-  uri = "https://syllabus.kosen-k.go.jp/Pages/PublicSchools"
+BASE_URL = "https://syllabus.kosen-k.go.jp"
 
+def request_parsed_html(uri) #nokogiriでパースしたhtmlを返す
   charset = nil
-
   html = open(uri) do |f|
     charset = f.charset
     f.read
   end
+  return Nokogiri::HTML.parse(html, nil, charset)
+end
 
-  doc = Nokogiri::HTML.parse(html, nil, charset)
-  doc.xpath('//div[@style=""]/div[@class="btn btn-default"]').each do |node|
+def get_school
+  uri = "https://syllabus.kosen-k.go.jp/Pages/PublicSchools"
+  doc = request_parsed_html(uri)
+
+  doc.xpath('//div[not(contains(@style, "display:none"))]/div[@class="btn btn-default"]').each do |node|
     node.css('a').each do |link|
-      school_id = link[:href][35,2]
-      uri = "https://syllabus.kosen-k.go.jp" + link[:href]
-      #puts %(getting #{uri})
-      get_department_id(uri, school_id)
+      next_uri = BASE_URL + link[:href]
+      get_department_id(next_uri)
     end
   end
 end
 
-def get_department_id(uri, school_id)
-  #puts %(getting department_id)
-  charset = nil
-
+def get_department_id(uri)
   department_name = String.new
-  department_id = String.new 
 
-  html = open(uri) do |f|
-    charset = f.charset
-    f.read
-  end
-
-  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc = request_parsed_html(uri)
   file_name = doc.at('//h1').inner_text
-  doc.xpath('//body/div/div/div[@class="row"]/div/div[@class="panel panel-default"]/div[@class="panel-body"]/div/div[@class="row"]/div[@class="col-md-6"]').each do |node|
+
+  doc.xpath('//div[@class="row"]/div[@class="col-md-6"]').each do |node|
     if node.xpath('h4[@class="list-group-item-heading"]').inner_text.empty?
       next
     else
       department_name = node.xpath('h4[@class="list-group-item-heading"]').inner_text
     end
     node.parent.css('a').each do |link|
-
       if link[:href].include?("Subjects")
-        uri= "https://syllabus.kosen-k.go.jp" + link[:href]
-        #puts %(getting #{uri})
-        get_subject_code(uri, file_name)
+        next_uri= BASE_URL + link[:href]
+        get_subject_code(next_uri, file_name)
       else
         next
       end
@@ -60,42 +51,26 @@ def get_department_id(uri, school_id)
 end
 
 def get_subject_code(uri, file_name)
-  %(getting subjct code)
-
   charset = nil
 
-  titles = []
-
-  html = open(uri) do |f|
-    charset = f.charset
-    f.read
-  end
-
-  doc = Nokogiri::HTML.parse(html, nil, charset)
+  doc = request_parsed_html(uri)
   file_name += ("_" + doc.at('//h1').inner_text)
   doc.xpath('//tr[@class="course- "]/td').each do |node|
     node.xpath('div[@class="subject-item"]').each do |child_node|
       child_node.css('a').each do |link|
-        uri = URI.encode("https://syllabus.kosen-k.go.jp" + (link[:href].gsub(/\\u([\da-fA-F]{4})/) { [$1].pack('H*').unpack('n*').pack('U*') }))
-        get_bg_success(uri, file_name)
+        next_uri = URI.encode(BASE_URL + (link[:href].gsub(/\\u([\da-fA-F]{4})/) { [$1].pack('H*').unpack('n*').pack('U*') }))
+        get_bg_success(next_uri, file_name)
       end
     end
   end
 end
 
 def get_bg_success(uri,file_name)
-  #puts %(getting bg duccess)
-  charset = nil
-
-  html = open(uri) do |f|
-    charset = f.charset
-    f.read
-  end
-
   row = Array.new(2)
   column = Array.new(1)
   subject_title = Array.new
-  doc = Nokogiri::HTML.parse(html, nil, charset)
+
+  doc = request_parsed_html(uri)
   doc.xpath('//table[@id="MainContent_SubjectSyllabus_wariaiTable"]').each do |node|
     node.xpath('tr[@class="bg-success"]/th').each do |child_node|
       row << child_node.inner_text
@@ -114,7 +89,7 @@ def get_bg_success(uri,file_name)
       end
     end
   end
-  #puts file_name
+  
   CSV.open("./shirabasu/#{file_name}.csv", "a") do |csv|
     csv << [subject_title]
     csv << row
@@ -128,7 +103,3 @@ end
 Benchmark.bm do |bm|
   bm.report { get_school }
 end
-
-#get_subject_code("https://syllabus.kosen-k.go.jp/Pages/PublicSubjects?school_id=40&department_id=13&year=2018", "test")
-
-#puts URI.encode("https://syllabus.kosen-k.go.jp" + ("/Pages/PublicSyllabus?school_id=40&department_id=13&subject_code=104610（後期）&year=2018".gsub(/\\u([\da-fA-F]{4})/) { [$1].pack('H*').unpack('n*').pack('U*') }))
